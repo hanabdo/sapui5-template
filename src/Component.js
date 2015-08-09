daa8a8510f1660f62b34979b5cb127548519625e
krasnobaev <alekseykrasnobaev@gmail.com>
2015-08-10 01:04:16 +0300

    v0.0.2 started

--- src/Component.js
+++ src/Component.js
@@ -10,7 +10,7 @@ jQuery.sap.require('sampleapp.TopRouter');
 sap.ui.core.UIComponent.extend('sampleapp.Component', {
   metadata: {
     name: 'SAPUI5 sample application',
-    version: '0.0.1',
+    version: '0.0.2',
     includes: [],
     dependencies: {
       libs: ['sap.m'],
